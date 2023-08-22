7e39ac4e059927a103653d932b0764b2d3025320
Sarah Meyer <sarah@macbook-pro-von-sarah.home>
2023-08-22 20:04:32 +0200

    initial commit

---
+++ functions.py
@@ -0,0 +1,7 @@
+def my_function(age):
+  if age > 18:
+      print("You are an adult")
+      return True
+  else:
+      print("You're not an adult yet")
+      return False
---
+++ test.py
@@ -0,0 +1,20 @@
+result = 1+5
+age = 26
+print("Hello World")
+print(result)
+print("My age is: " +str(age))
+
+import functions
+
+
+
+result = functions.my_function(age)
+print(result)
+
+a = 0
+b = 1
+while (a+b <200):
+  a = b
+  b = a + b 
+  
+  print(a)
