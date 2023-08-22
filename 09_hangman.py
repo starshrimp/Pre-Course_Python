a26b04fd96e6d75788d0f98ca4e074f3fbd33545
Sarah Meyer <sarah@macbook-pro-von-sarah.home>
2023-08-22 20:25:21 +0200

    added hangman drawing

--- 09_hangman.py
+++ 09_hangman.py
@@ -10,7 +10,58 @@ else:
 
 positions = []
 guessed_letters = []
-attempts = 6  # Number of allowed wrong attempts
+attempts = 7  # Number of allowed wrong attempts
+hangman_pictures = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+hangman_pictures = hangman_pictures[::-1] # Reverse the list
 
 while True:  
   if not all(letter in guessed_letters for letter in random_word):
@@ -18,7 +69,7 @@ while True:
   elif all(letter in guessed_letters for letter in random_word):
     print("Congratulations! You have won the game!")
     break
-  if attempts == 6 and not guessed_letters:
+  if attempts == 7 and not guessed_letters:
     print(len(random_word)*"_")
   
   user_input=input().upper()
@@ -29,17 +80,18 @@ while True:
   
   if user_input in random_word:
     if user_input in random_word:
-          print("Correct!")
-          guessed_letters.append(user_input)
-          print(" ".join(letter if letter in guessed_letters else "_" for letter in random_word))
-          for letter in random_word:
-            # Check if the current letter is in the guessed_letters collection
-            if letter in guessed_letters:
-                # If the letter is guessed, add it to the displayed_characters list
-                displayed_characters.append(letter)
-            else:
-                # If the letter is not guessed, add an underscore to the displayed_characters list
-                displayed_characters.append("_")
+        print("Correct!")
+        guessed_letters.append(user_input)
+        print(" ".join(letter if letter in guessed_letters else "_" for letter in random_word))
+        for letter in random_word:
+          # Check if the current letter is in the guessed_letters collection
+          if letter in guessed_letters:
+              # If the letter is guessed, add it to the displayed_characters list
+              displayed_characters.append(letter)
+          else:
+              # If the letter is not guessed, add an underscore to the displayed_characters list
+              displayed_characters.append("_")
+          
   elif user_input == 'quit':
         print("Exiting the loop...")
         break
@@ -50,6 +102,8 @@ while True:
   elif attempts == 1:
         print("You have no more attempts left. You lose!")
         print("Random word:", random_word)
+        attempts -= 1
+        print(hangman_pictures[attempts])
         break
   else:
     print(f"The letter '{user_input}' is not in the string.")
@@ -57,7 +111,8 @@ while True:
     if attempts == 1:
       print(f"You have {attempts} attempt left.")
     else:
-       print(f"You have {attempts} attempts left.")
+      print(f"You have {attempts} attempts left.")
+    print(hangman_pictures[attempts])
     print(" ".join(letter if letter in guessed_letters else "_" for letter in random_word))
 
 
